b841bb92e64b2f37b5c16c30b56e161f13a2d328
stedewa80 <d-github@rippe.xyz>
2026-05-19 21:44:32 +0000

    Deploying to gh-pages from @ stedewa80/cornertime@468b08cb6e83850d8b070a8a4ad3033d013652bb 🚀

--- precache-manifest.7c4c3baa56197ff295c446dbf16cfecf.js
+++ precache-manifest.7c4c3baa56197ff295c446dbf16cfecf.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2cf22bd6c10b7a2e9ecace558323e722",
+    "revision": "1bd9193c59be6fc490f2c883fc99a95d",
     "url": "/cornertime/index.html"
   },
   {
-    "revision": "15499489439784dec8f7",
+    "revision": "336795c7e80815e0667f",
     "url": "/cornertime/static/css/2.0cee8fd6.chunk.css"
   },
   {
-    "revision": "e9cdf805a3024a0f07e6",
+    "revision": "e1460a6522403c99f00a",
     "url": "/cornertime/static/css/main.bd4995d1.chunk.css"
   },
   {
-    "revision": "15499489439784dec8f7",
-    "url": "/cornertime/static/js/2.065f1b1f.chunk.js"
+    "revision": "336795c7e80815e0667f",
+    "url": "/cornertime/static/js/2.c8e952e3.chunk.js"
   },
   {
     "revision": "809f8196affeaac0e5176b72b79c267c",
-    "url": "/cornertime/static/js/2.065f1b1f.chunk.js.LICENSE.txt"
+    "url": "/cornertime/static/js/2.c8e952e3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e9cdf805a3024a0f07e6",
-    "url": "/cornertime/static/js/main.f29f2102.chunk.js"
+    "revision": "e1460a6522403c99f00a",
+    "url": "/cornertime/static/js/main.7b120b6a.chunk.js"
   },
   {
     "revision": "3c85126fc35e7928a26b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cornertime/precache-manifest.cdbd9ab806911a06276a4fae7953f3b2.js"
+  "/cornertime/precache-manifest.7c4c3baa56197ff295c446dbf16cfecf.js"
 );
 
 self.addEventListener('message', (event) => {
